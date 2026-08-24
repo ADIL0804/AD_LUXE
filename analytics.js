cf863b38d2b68010aa62e8892710e3f1af62a2c9
ADIL0804 <mrfou739@gmail.com>
2026-08-24 16:01:17 +0100

    Load compact catalog layout

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-document.write('<script src="/analytics-core.js?v=20260824-3"><\/script><script src="/upgrade.js?v=20260824-2"><\/script><script src="/seo-runtime.js?v=20260824-3"><\/script>');
+document.write('<link rel="stylesheet" href="/catalog-compact.css?v=20260824-1"><script src="/analytics-core.js?v=20260824-3"><\/script><script src="/upgrade.js?v=20260824-2"><\/script><script src="/seo-runtime.js?v=20260824-3"><\/script>');
