16f88db813e8de1049ba99a697a10be49a65b0a8
ADIL0804 <mrfou739@gmail.com>
2026-08-24 13:51:42 +0100

    Load complete SEO and checkout runtime

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-document.write('<script src="/analytics-core.js?v=20260824-1"><\/script><script src="/upgrade.js?v=20260824-1"><\/script>');
+document.write('<script src="/analytics-core.js?v=20260824-1"><\/script><script src="/upgrade.js?v=20260824-1"><\/script><script src="/seo-runtime.js?v=20260824-2"><\/script>');
